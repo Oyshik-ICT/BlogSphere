081d8e637f220a1ee53050321d9007c88e64f609
Oyshik-ICT <oyshikf@gmail.com>
2024-04-30 17:29:19 +0600

    Modified 2

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -25,6 +25,7 @@ class UserPostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
 
+
     def get_queryset(self):
         user = get_object_or_404(User, username = self.kwargs.get('username'))
         return Post.objects.filter(author = user).order_by('-date_posted')
